d1988f29dd032916b6ab9da86ee431606f8c8e80
coryjfowler <cory.j.fowler@gmail.com>
2013-10-02 06:15:51 -0400

    Update receive.ino
    Added getCanId() function into example and reformatted the serial output.

--- example/receive/receive.ino
+++ example/receive/receive.ino
@@ -2,30 +2,32 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
-unsigned char Flag_Recv = 0;
+long unsigned int rxId;
 unsigned char len = 0;
-unsigned char buf[8];
-char str[20];
+unsigned char rxBuf[8];
+
 
 void setup()
 {
+  Serial.begin(115200);
   CAN.begin(CAN_500KBPS);                       // init can bus : baudrate = 500k 
   pinMode(2, INPUT);                            // Setting pin 2 for /INT input
-  Serial.begin(115200);
+  Serial.println("MCP2515 Library Receive Example...")
 }
 
 void loop()
 {
-    if(!digitalRead(2))                         // check if get data
+    if(!digitalRead(2))                         // If pin 2 is low, read receive buffer
     {
-      CAN.readMsgBuf(&len, buf);                // read data,  len: data length, buf: data buf
-      Serial.println("CAN_BUS GET DATA!");
-      Serial.print("data len = ");
-      Serial.println(len);
-      
+      CAN.readMsgBuf(&len, rxBuf);              // Read data: len = data length, buf = data byte(s)
+      rxId = CAN.getCanId();                    // Get message ID
+      Serial.print("ID: ");
+      Serial.print(rxId, HEX);
+      Serial.print("  Data: ");
       for(int i = 0; i<len; i++)                // print the data
       {
-        Serial.print(buf[i]);Serial.print("\t");
+        Serial.print(rxBuf[i], HEX);
+        Serial.print(" ");
       }
       Serial.println();
     }
