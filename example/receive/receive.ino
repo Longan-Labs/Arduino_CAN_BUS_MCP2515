47c933c90fa7af4b24bff1611f6c0fc8f7937fd3
coryjfowler <cory.j.fowler@gmail.com>
2013-10-02 00:16:00 -0400

    Update receive.ino
    Fixed /INT handling.
    Code would work itself into an infinite loop when on a busy CAN bus.  The /INT line is held low when the receive buffer is full which would prevent the interrupt handler from every setting the flag again.  The code now reads from the MCP2515 whenever the /INT line is in the low state.

--- example/receive/receive.ino
+++ example/receive/receive.ino
@@ -9,27 +9,21 @@ char str[20];
 
 void setup()
 {
-  CAN.begin(CAN_500KBPS);                       // init can bus : baudrate = 500k
-  attachInterrupt(0, MCP2515_ISR, FALLING);     // start interrupt
+  CAN.begin(CAN_500KBPS);                       // init can bus : baudrate = 500k 
+  pinMode(2, INPUT);                            // Setting pin 2 for /INT input
   Serial.begin(115200);
-}
-
-void MCP2515_ISR()
-{
-    Flag_Recv = 1;
 }
 
 void loop()
 {
-    if(Flag_Recv)                           // check if get data
+    if(!digitalRead(2))                         // check if get data
     {
-      Flag_Recv = 0;                        // clear flag
-      CAN.readMsgBuf(&len, buf);            // read data,  len: data length, buf: data buf
+      CAN.readMsgBuf(&len, buf);                // read data,  len: data length, buf: data buf
       Serial.println("CAN_BUS GET DATA!");
       Serial.print("data len = ");
       Serial.println(len);
       
-      for(int i = 0; i<len; i++)            // print the data
+      for(int i = 0; i<len; i++)                // print the data
       {
         Serial.print(buf[i]);Serial.print("\t");
       }
