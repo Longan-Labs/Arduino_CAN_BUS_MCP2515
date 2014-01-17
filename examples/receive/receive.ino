ba3a458349e7ab25fc56d2c362a6835161f35697
Cory <coryjfowler@users.noreply.github.com>
2014-01-17 15:13:18 -0500

    Updated to retain compatibility...

--- examples/receive/receive.ino
+++ examples/receive/receive.ino
@@ -6,11 +6,13 @@ long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
 
+MCP_CAN CAN0(10);                               // Set CS to pin 10
+
 
 void setup()
 {
   Serial.begin(115200);
-  CAN.begin(CAN_500KBPS);                       // init can bus : baudrate = 500k 
+  CAN0.begin(CAN_500KBPS);                       // init can bus : baudrate = 500k 
   pinMode(2, INPUT);                            // Setting pin 2 for /INT input
   Serial.println("MCP2515 Library Receive Example...");
 }
@@ -19,8 +21,8 @@ void loop()
 {
     if(!digitalRead(2))                         // If pin 2 is low, read receive buffer
     {
-      CAN.readMsgBuf(&len, rxBuf);              // Read data: len = data length, buf = data byte(s)
-      rxId = CAN.getCanId();                    // Get message ID
+      CAN0.readMsgBuf(&len, rxBuf);              // Read data: len = data length, buf = data byte(s)
+      rxId = CAN0.getCanId();                    // Get message ID
       Serial.print("ID: ");
       Serial.print(rxId, HEX);
       Serial.print("  Data: ");
