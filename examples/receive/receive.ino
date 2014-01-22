ad87ed06e51db9612f31eff5105c5629cc2274b1
Cory <coryjfowler@users.noreply.github.com>
2014-01-22 03:06:42 -0500

    Update receive.ino

--- examples/receive/receive.ino
+++ examples/receive/receive.ino
@@ -12,7 +12,8 @@ MCP_CAN CAN0(10);                               // Set CS to pin 10
 void setup()
 {
   Serial.begin(115200);
-  CAN0.begin(CAN_500KBPS);                       // init can bus : baudrate = 500k 
+  CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ);  // init CAN Bus with 500kb/s baudrate at 16MHz with Mask & Filters disabled
+  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
   pinMode(2, INPUT);                            // Setting pin 2 for /INT input
   Serial.println("MCP2515 Library Receive Example...");
 }
@@ -21,10 +22,12 @@ void loop()
 {
     if(!digitalRead(2))                         // If pin 2 is low, read receive buffer
     {
-      CAN0.readMsgBuf(&len, rxBuf);              // Read data: len = data length, buf = data byte(s)
-      rxId = CAN0.getCanId();                    // Get message ID
+      CAN0.readMsgBuf(&rxId, &len, rxBuf);              // Read data: len = data length, buf = data byte(s)
+//      rxId = CAN0.getCanId();              // Function will be depreciated soon due to readMsgBuf now returning ID
+
       Serial.print("ID: ");
       Serial.print(rxId, HEX);
+
       Serial.print("  Data: ");
       for(int i = 0; i<len; i++)                // Print each byte of the data
       {
@@ -35,6 +38,7 @@ void loop()
         Serial.print(rxBuf[i], HEX);
         Serial.print(" ");
       }
+
       Serial.println();
     }
 }
