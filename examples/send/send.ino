188144e0a66e8a8bb8ed39ce505a6d249c3100c4
Cory <coryjfowler@users.noreply.github.com>
2014-01-22 03:09:35 -0500

    Update send.ino

--- examples/send/send.ino
+++ examples/send/send.ino
@@ -7,9 +7,10 @@ MCP_CAN CAN0(10);                                      // Set CS to pin 10
 void setup()
 {
   Serial.begin(115200);
-  // init can bus, baudrate: 500k
-  if(CAN0.begin(CAN_500KBPS) == CAN_OK) Serial.print("can init ok!!\r\n");
+  // Init CAN bus with 500kb/s baudrate at 16MHz with Mask and Filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) Serial.print("MCP2515 init ok!!\r\n");
   else Serial.print("Can init fail!!\r\n");
+  CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
 }
 
 unsigned char stmp[8] = {0, 1, 2, 3, 4, 5, 6, 7};
