c9452a9a2e3782b86205d324bc5b3844ec8c0d52
Cory <coryjfowler@users.noreply.github.com>
2014-09-22 23:13:54 -0400

    Updated Bit Rate Definitions

--- mcp_can_dfs.h
+++ mcp_can_dfs.h
@@ -426,18 +426,20 @@
 #define MCP_16MHZ    1
 #define MCP_8MHZ     2
 
+#define CAN_4K096BPS 0
 #define CAN_5KBPS    1
 #define CAN_10KBPS   2
 #define CAN_20KBPS   3
-#define CAN_40KBPS   4
-#define CAN_50KBPS   5
-#define CAN_80KBPS   6
-#define CAN_100KBPS  7
-#define CAN_125KBPS  8
-#define CAN_200KBPS  9
-#define CAN_250KBPS  10
-#define CAN_500KBPS  11
-#define CAN_1000KBPS 12
+#define CAN_31K25BPS 4
+#define CAN_40KBPS   5
+#define CAN_50KBPS   6
+#define CAN_80KBPS   7
+#define CAN_100KBPS  8
+#define CAN_125KBPS  9
+#define CAN_200KBPS  10
+#define CAN_250KBPS  11
+#define CAN_500KBPS  12
+#define CAN_1000KBPS 13
 
 #define CAN_OK             (0)
 #define CAN_FAILINIT       (1)
