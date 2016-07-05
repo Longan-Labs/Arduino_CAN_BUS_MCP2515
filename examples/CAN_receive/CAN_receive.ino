6853d17a9740f3cb6efc61dbb71fde046d7f3a48
Cory <coryjfowler@users.noreply.github.com>
2016-07-05 11:24:54 -0400

    Update CAN_receive.ino
    Made serial output more useful as a CAN sniffing tool and to demonstrate using the extra bits of the ID.

--- examples/CAN_receive/CAN_receive.ino
+++ examples/CAN_receive/CAN_receive.ino
@@ -28,26 +28,29 @@ void setup()
 
 void loop()
 {
-    if(!digitalRead(2))                         // If pin 2 is low, read receive buffer
-    {
-      CAN0.readMsgBuf(&rxId, &len, rxBuf);              // Read data: len = data length, buf = data byte(s)
-
-      Serial.print("ID: ");                     // Print the message ID.
-      Serial.print(rxId, HEX);
-
-      Serial.print("  Data: ");
-      for(int i = 0; i<len; i++)                // Print each byte of the data.
-      {
-        if(rxBuf[i] < 0x10)                     // If data byte is less than 0x10, add a leading zero.
-        {
-          Serial.print("0");
-        }
-        Serial.print(rxBuf[i], HEX);
-        Serial.print(" ");
+  if(!digitalRead(2))                         // If pin 2 is low, read receive buffer
+  {
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+    
+    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+    else
+      sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+  
+    Serial.print(msgString);
+  
+    if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      Serial.print(msgString);
+    } else {
+      for(byte i = 0; i<len; i++){
+        sprintf(msgString, " 0x%.2X", rxBuf[i]);
+        Serial.print(msgString);
       }
-
-      Serial.println();
     }
+        
+    Serial.println();
+  }
 }
 
 /*********************************************************************************************************
