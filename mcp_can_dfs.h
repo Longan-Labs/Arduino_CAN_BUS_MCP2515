59e64f54e0edcee44b2a9604d1e96fedd02eb7b1
Cory <coryjfowler@users.noreply.github.com>
2016-10-04 11:18:37 -0400

    Added 33k3bps for 8MHz and 16MHz

--- mcp_can_dfs.h
+++ mcp_can_dfs.h
@@ -439,15 +439,16 @@
 #define CAN_10KBPS   2
 #define CAN_20KBPS   3
 #define CAN_31K25BPS 4
-#define CAN_40KBPS   5
-#define CAN_50KBPS   6
-#define CAN_80KBPS   7
-#define CAN_100KBPS  8
-#define CAN_125KBPS  9
-#define CAN_200KBPS  10
-#define CAN_250KBPS  11
-#define CAN_500KBPS  12
-#define CAN_1000KBPS 13
+#define CAN_33K3BPS  5
+#define CAN_40KBPS   6
+#define CAN_50KBPS   7
+#define CAN_80KBPS   8
+#define CAN_100KBPS  9
+#define CAN_125KBPS  10
+#define CAN_200KBPS  11
+#define CAN_250KBPS  12
+#define CAN_500KBPS  13
+#define CAN_1000KBPS 14
 
 #define CAN_OK             (0)
 #define CAN_FAILINIT       (1)
