7508cef328098087bb80b9cdfc69ff630d6fb0ce
Stephen Lo <36740855+stephen1874@users.noreply.github.com>
2021-05-11 14:16:08 +0800

    Update send.ino

--- examples/send/send.ino
+++ examples/send/send.ino
@@ -1,3 +1,49 @@
+# Longan Labs CAN Bus Library 
+
+[![Actions Status](https://github.com/arduino/arduino-cli-example/workflows/test/badge.svg)](https://github.com/arduino/arduino-cli-example/actions)
+[![Spell Check](https://github.com/arduino/compile-sketches/workflows/Spell%20Check/badge.svg)](https://github.com/arduino/compile-sketches/actions?workflow=Spell+Check)
+[![codecov](https://codecov.io/gh/arduino/compile-sketches/branch/main/graph/badge.svg?token=Uv6f1ebMZ4)](https://codecov.io/gh/arduino/compile-sketches)
+
+Arduino library for MCP2515, it's available for most of theArduino boards, we test it with Arduino UNO, Leonardo, Mega as well as Zero.
+
+With this library, you can,
+
+1. Send a CAN2.0 frame
+2. Receive a CAN2.0 frame
+3. Get data from OBD-II
+4. Set the masks and filters, there're 32 masks and filters. 
+
+## Installation
+
+1. [Download the library](https://github.com/Longan-Labs/Aruino_CAN_BUS_MCP2515/archive/refs/heads/master.zip)
+2. Extract the zip file
+3. In the Arduino IDe, navigate to Sketch > Include Library > Add .ZIP Library
+
+## Respository Contents
+
+* [**/examples**](./examples) - Example sketches for the library (.ino). Run these from the Arduino IDE.
+* [**/src**](./src) - Source files for the library (.cpp, .h).
+* [**keywords.txt**](./keywords.txt) - Keywords from this library that will be highlighted in the Arduino IDE.
+* [**library.properties**](./library.properties) - General library properties for the Arduino package manager.
+
+## Functions
+
+- begin()
+- init_Mask()
+- init_filt()
+- checkReceive()
+- readMsgBufID()
+- readMsgBuf()
+- getCanId()
+- sendMsgBuf()
+- isRemoteRequest()
+- isExtendedFrame()
+
+## Examples
+
+here are many examples implemented in this library. One of the examples is below. You can find other examples [here](./examples)
+
+```Cpp
 /*  send a frame from can bus
     support@longan-labs.cc
     
@@ -54,25 +100,54 @@ void setup()
     Serial.println("CAN BUS OK!");
 }
 
-unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char stmp[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 void loop()
 {
-    // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
-    stmp[7] = stmp[7]+1;
-    if(stmp[7] == 100)
-    {
-        stmp[7] = 0;
-        stmp[6] = stmp[6] + 1;
-        
-        if(stmp[6] == 100)
-        {
-            stmp[6] = 0;
-            stmp[5] = stmp[6] + 1;
-        }
-    }
-    
     CAN.sendMsgBuf(0x00, 0, 8, stmp);
     delay(100);                       // send data per 100ms
 }
 
 // END FILE
+```
+
+## Get a Dev Board
+
+If you need a Dev board, plese try,
+
+- [CAN Bus Shield for Arduino](https://www.longan-labs.cc/1030016.html)
+- [CANBed V1](https://www.longan-labs.cc/1030008.html)
+- [CANBed M0](https://www.longan-labs.cc/1030014.html)
+- [OBD-II CAN Bus GPS Dev Kit](https://www.longan-labs.cc/1030003.html)
+
+## License
+
+```
+MIT License
+
+Copyright (c) 2018 @ Longan Labs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+## Contact us
+
+If you have any question, please feel free to contact [support@longan-labs.cc](support@longan-labs.cc)
+
+
+[![Analytics](https://ga-beacon.appspot.com/UA-101965714-1/Longan_CANFD)](https://github.com/igrigorik/ga-beacon)
