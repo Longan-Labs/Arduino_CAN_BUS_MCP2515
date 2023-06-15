dab6dfe8d88de66c2b80282194099a6fcf6de7c2
Stephen Lo <36740855+stephen1874@users.noreply.github.com>
2023-06-15 19:20:05 +0800

    Update mcp_can.cpp

--- mcp_can.cpp
+++ mcp_can.cpp
@@ -749,7 +749,7 @@ byte MCP_CAN::setMsg(unsigned long id, byte ext, byte len, byte rtr, byte *pData
     ext_flg     = ext;
     can_id      = id;
     //dta_len     = min(len, MAX_CHAR_IN_MESSAGE);
-    dta len = len > MAX CHAR IN MESSAGE ? MAX CHAR IN MESSAGE : len;
+    dta len = len > MAX_CHAR_IN_MESSAGE ? MAX_CHAR_IN_MESSAGE: len;
     rtr         = rtr;
     for(int i = 0; i<dta_len; i++)
     {
